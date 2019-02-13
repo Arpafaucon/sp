ed32edd51270acfb941a01c75f5165309f4892df
Grégoire Roussel [LIX] <arpafaucon@gmail.com>
2019-02-13 16:48:56 +0100

    removed crazyflie copied from cf_gazebo. created aux module

--- sp_mate/src/lookout.py
+++ sp_mate/src/lookout.py
@@ -4,10 +4,14 @@ Lookout node
 """
 
 import rospy
+
+from geometry_msgs.msg import PoseStamped
+from tf.transformations import quaternion_from_euler
 from sp_core.msg import SwarmAllocation, SwarmPosition
-from sp_mate.srv import DroneLocation, DroneLocationRequest, DroneLocationResponse
+from sp_mate.srv import DronePosition, DronePositionRequest, DronePositionResponse
 from crazyflie_driver.msg import GenericLogData
 
+from rospools import PublisherPool
 
 class Lookout:
     def __init__(self):
@@ -23,12 +27,15 @@ class Lookout:
         self.swarm_pos_seq = 0
 
         # position service
-        self.drone_pos_srv = rospy.Service("/sp/drone_position", DroneLocation, self._srv_drone_location)
+        self.drone_pos_srv = rospy.Service("/sp/drone_position", DronePosition, self._srv_drone_location)
+
+        # position publishers
+        self.pose_pubpool = PublisherPool(PoseStamped, "/pose_stamped")
 
     
     def _srv_drone_location(self, req):
         x, y, z = self._get_drone_pos(req.active_id)
-        res = DroneLocationResponse()
+        res = DronePositionResponse()
         res.position = [x, y, z]
         return res
 
@@ -42,31 +49,46 @@ class Lookout:
         ad_namespace = self.swarm_alloc_msg.active_drones_namespaces[drone_active_id]
         ad_position_topic = "/{}/local_position".format(ad_namespace)
         local_position_msg = rospy.wait_for_message(ad_position_topic, GenericLogData)
-        x = local_position_msg.values[0]
-        y = local_position_msg.values[1]
-        z = local_position_msg.values[2]
+        # x = local_position_msg.values[0]
+        # y = local_position_msg.values[1]
+        # z = local_position_msg.values[2]
         # r = local_position_msg.values[3]
         # p = local_position_msg.values[4]
-        # y = local_position_msg.values[5]
-        return x, y, z
+        # yaw = local_position_msg.values[5]
+        return local_position_msg.values, ad_namespace
+
+    def _pub_pose_stamped(self, namespace, x, y, z, roll, pitch, yaw):
+        pub = self.pose_pubpool.get_pub(namespace)
+        pos = PoseStamped()
+        pos.position.x = x
+        pos.position.y = y
+        pos.position.z = z
+
+        pos.header.frame_id = "/map"
+        pos.header.stamp = rospy.Time.now()
+        pos.orientation = quaternion_from_euler(roll, pitch, yaw)
+
+        pub.publish(pos)
     
     def  _get_swarm_pos(self):
         xl = []
         yl = []
         zl = []
+        yawl = []
+        swarm_pos = []
         for ad_id in range(self.swarm_alloc_msg.num_drones_active):
-            x, y, z = self._get_drone_pos(ad_id)
-            xl.append(x)
-            yl.append(y)
-            zl.append(z)
-        return xl, yl, zl
+            position, ns = self._get_drone_pos(ad_id)
+            swarm_pos.append( (position, ns))
+        return swarm_pos
 
-    def _publish_swarm_pos(self):
+    def _publish_swarm_pos(self, swarm_pos):
         if self.swarm_alloc_msg is None:
             rospy.logwarn_throttle(1, "Lookout hasn't received swarm allocation. Cannot publish positions")
             return
 
-        xl, yl, zl = self._get_swarm_pos()
+        for drone_id in range(len(swarm_pos)):
+            
+
         swarm_position_msg = SwarmPosition()
         swarm_position_msg.header.stamp = rospy.Time.now()
         swarm_position_msg.header.seq = self.swarm_pos_seq
@@ -77,6 +99,7 @@ class Lookout:
         swarm_position_msg.x = xl
         swarm_position_msg.y = yl
         swarm_position_msg.z = zl
+        swarm_position_msg.yaw = yl
 
         self.swarm_pos_pub.publish(swarm_position_msg)
         rospy.loginfo("published swarm positions")
---
+++ sp_mate/src/rospools.py
@@ -0,0 +1,33 @@
+import rospy
+
+class PublisherPool:
+    def __init__(self, Message_Class, topic_suffix, queue_size=10):
+        self.pool = dict()
+        self.MsgClass = Message_Class
+        self.topic_suffix = topic_suffix
+        self.queue_size = queue_size
+
+    def get_pub(self, namespace):
+        if namespace in self.pool:
+            return self.pool[namespace]
+        else:
+            topic = '{}/{}'.format(namespace, self.topic_suffix)
+            pub = rospy.Publisher(topic, self.MsgClass, queue_size=self.queue_size=)
+            self.pool[namespace] = pub
+            return pub
+
+class ServiceProxyPool:
+    def __init__(self, Srv_class, srv_suffix):
+        self.pool = dict()
+        self.MsgClass = Srv_class
+        self.srv_suffix = srv_suffix
+
+    def get_pub(self, namespace):
+        if namespace in self.pool:
+            return self.pool[namespace]
+        else:
+            topic = '{}/{}'.format(namespace, self.srv_suffix)
+            rospy.wait_for_service(topic)
+            srv = rospy.ServiceProxy(topic, self.MsgClass)
+            self.pool[namespace] = srv
+            return srv
