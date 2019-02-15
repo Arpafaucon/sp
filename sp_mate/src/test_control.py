d740393986b65c0fd0ee7a291fe99731b31e6083
Grégoire Roussel <arpafaucon@gmail.com>
2019-02-15 16:57:21 +0100

    switched to light crazyflie

--- sp_mate/src/test_control.py
+++ sp_mate/src/test_control.py
@@ -6,21 +6,21 @@ from sp_core.msg import CaptainOrders
 def test_control():
     rospy.init_node("test_control")
     pub = rospy.Publisher(CAPTAIN_ORDERS_SUB, CaptainOrders, queue_size=10)
-    rate = rospy.Rate(.05)
-
-    cm = CaptainOrders()
-    cm.num_drones = 2
-    cm.waypoints_start_indices = [0,1]
-    cm.waypoints_stop_indices = [1, 2]
-    cm.waypoints_x = [0,1]
-    cm.waypoints_y = [0,0]
+    rate = rospy.Rate(.1)
 
 #     cm = CaptainOrders()
 #     cm.num_drones = 2
-#     cm.waypoints_start_indices = [0,3]
-#     cm.waypoints_stop_indices = [3, 6]
-#     cm.waypoints_x = [2, 5, 2, 0, 0, 0]
-#     cm.waypoints_y = [2, 2, 2, 1, 3, 1]
+#     cm.waypoints_start_indices = [0,1]
+#     cm.waypoints_stop_indices = [1, 2]
+#     cm.waypoints_x = [0,1]
+#     cm.waypoints_y = [0,0]
+
+    cm = CaptainOrders()
+    cm.num_drones = 2
+    cm.waypoints_start_indices = [0,4]
+    cm.waypoints_stop_indices = [4, 8]
+    cm.waypoints_x = [0, 1, 1, 0, 2, 3, 3, 2]
+    cm.waypoints_y = [-1, -1, 0, 0, 1, 1, 2, 2]
 
     while not rospy.is_shutdown():
         pub.publish(cm)
